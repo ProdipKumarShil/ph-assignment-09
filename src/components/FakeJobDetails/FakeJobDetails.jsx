e57ac814445b247759c5c0d1516c7f40aff48ae2
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-04-13 20:09:38 +0600

    added navbar dropdown

--- src/components/FakeJobDetails/FakeJobDetails.jsx
+++ src/components/FakeJobDetails/FakeJobDetails.jsx
@@ -1,8 +1,12 @@
+import Lottie from 'lottie-react'
+import walk from '../../../public/toucan-walk-cycle.json'
 import React from 'react';
+import girlJob from '../../../public/job.json'
 
 const FakeJobDetails = () => {
   return (
     <div className='flex items-center justify-center w-[100vw] h-[100vh]'>
+      <Lottie animationData={walk} />
       <h1 className='text-5xl font-semibold text-center'>Please select a Job</h1>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Bars3BottomLeftIcon, Bars3CenterLeftIcon } from '@heroicons/react/24/outline'
+import { XMarkIcon, Bars3CenterLeftIcon } from '@heroicons/react/24/outline'
 
 
 const Navbar = () => {
+  const [navIcon, setNavIcon] = useState(true)
+  const changeState = () => {
+    setNavIcon(!navIcon)
+    console.log(navIcon)
+  }
   return (
-    <div className='max-w-7xl flex justify-between items-center my-5 mx-auto px-4'>
+    <div className='max-w-7xl flex justify-between items-center my-5 mx-auto px-4 relative'>
       <Link to='/' className='text-3xl font-semibold'>Bd Jobs</Link>
       <ul className='hidden md:inline-block'>
         <Link to='jobDetails' className='mr-4 text-lg font-medium'>Job Details</Link>
@@ -13,7 +18,20 @@ const Navbar = () => {
         <Link to='blogs' className='mr-4 text-lg font-medium'>Blogs</Link>
       </ul>
       <Link to='/' className='mBtn hidden md:flex justify-center items-center'>Star Applying</Link>
-      <Bars3CenterLeftIcon className='w-8 h-8 md:hidden cursor-pointer' />
+
+
+      {/* useState */}
+      <div className={`${navIcon ? '-top-96' : 'top-12'} absolute  w-11/12 mx-auto duration-300 bg-gray-200 p-8 rounded-lg shadow-lg md:hidden`}>
+        <div className=" ">
+          <ul className='md:hidden flex flex-col gap-3'>
+            <Link to='jobDetails' className='mr-4 text-lg font-medium'>Job Details</Link>
+            <Link to='yourJobs' className='mr-4 text-lg font-medium'>Your Jobs</Link>
+            <Link to='blogs' className='mr-4 text-lg font-medium'>Blogs</Link>
+          </ul>
+          <Link to='/' className='mBtn flex justify-center items-center mt-4'>Star Applying</Link>
+        </div>
+      </div>
+      {navIcon ? <Bars3CenterLeftIcon onClick={changeState} className='w-8 h-8 md:hidden cursor-pointer' /> : <XMarkIcon onClick={changeState} className='w-8 h-8 md:hidden cursor-pointer' />}
     </div>
   );
 };
